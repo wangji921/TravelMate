c5a2b34223f324dbb30e43b29c459af429642d2c
wangji <wangji921@gmail.com>
2017-11-07 21:19:33 +1300

    First Google Maps Application

--- TravelMate/AppDelegate.swift
+++ TravelMate/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyAFtyOmHHbOw5-jqGYR0racTRMb8mcPa9o")
+        GMSPlacesClient.provideAPIKey("AIzaSyAFtyOmHHbOw5-jqGYR0racTRMb8mcPa9o")
         return true
     }
 
--- TravelMate/ViewController.swift
+++ TravelMate/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
 
     override func didReceiveMemoryWarning() {
